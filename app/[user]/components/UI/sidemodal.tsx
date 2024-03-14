2e8f8bea1c0fa199b027a40b8b7ff1538b0ff4f2
0x4E43 <nimaic.dev@gmail.com>
2024-03-14 22:32:08 +0530

    Adding editor modal

--- app/[user]/components/UI/sidemodal.tsx
+++ app/[user]/components/UI/sidemodal.tsx
@@ -1,15 +1,16 @@
+import Editor from "./editor";
+
 export default function SideModal() {
   return (
     <>
-      <div className="modal-box">
+      <div className="modal-box h-1/2">
         <form method="dialog">
           {/* if there is a button in form, it will close the modal */}
           <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
             ✕
           </button>
         </form>
-        <h3 className="font-bold text-lg">Hello!</h3>
-        <p className="py-4">Press ESC key or click on ✕ button to close</p>
+        <Editor/>
       </div>
     </>
   );
