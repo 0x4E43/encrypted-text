a03dfc9617a53d2862cc85e9308cd6e8be14eff7
0x4E43 <nimaic.dev@gmail.com>
2024-03-15 22:34:17 +0530

    Using react-icon

--- app/[user]/components/UI/editor.tsx
+++ app/[user]/components/UI/editor.tsx
@@ -12,7 +12,7 @@ export default function Editor() {
           className="flex-1 w-full bg-inherit resize-none p-2"
         ></textarea>
         <div className="m-2">
-            <button className="btn btn-primary m-2">Save</button>
+            <span className="btn btn-primary m-2">Save</span>
             <button className="btn btn-ghost m-2">Cancel</button>
         </div>
       </div>
