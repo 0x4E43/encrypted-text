9389d81247c30b9b8ae97f0de0f0fc294cd5fc62
0x4E43 <nimaic.dev@gmail.com>
2024-03-16 23:10:42 +0530

    Submit button added

--- app/[user]/components/UI/sidemodal.tsx
+++ app/[user]/components/UI/sidemodal.tsx
@@ -3,10 +3,10 @@ import Editor from "./editor";
 export default function SideModal() {
   return (
     <>
-      <div className="modal-box h-1/2">
+      <div className="modal-box h-4/5 w-3/5">
         <form method="dialog">
           {/* if there is a button in form, it will close the modal */}
-          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 text-red-500">
             ✕
           </button>
         </form>
