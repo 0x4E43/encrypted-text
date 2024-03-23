50ac9ea1e65cb267aa7f6037f2fef0153459ff8b
0x4E43 <nimaic.dev@gmail.com>
2024-03-23 17:19:33 +0530

    Added API to add notes data

--- app/api/content/route.ts
+++ app/api/content/route.ts
@@ -1,6 +1,48 @@
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
 
 
+const prisma = new PrismaClient();
+
+type SiteData = {
+    siteName: string;
+    title: string;
+    content: string;
+}
 export async function POST(req: Request) {
-    const reqData = await req.json();
+    const reqData :  SiteData = await req.json();
+    if(reqData === undefined) {
+        return NextResponse.json({"error": "No data found"});
+    }
     const siteName = reqData.siteName;
+        try {
+            const site = await prisma.sites.findUnique({
+                where: {
+                    name: siteName,
+                },
+                });
+            
+            if(site) {
+                const result = await prisma.notes.create({
+                    data: {
+                        category: 'default', 
+                        title: reqData.title,
+                        content: reqData.content,
+                        siteId: site.id,
+                        createdAt: new Date(),
+                        updatedAt: new Date()
+                    }
+                });
+                return NextResponse.json({"message": "Data saved successfully", "data": result});
+
+            }else{
+                return NextResponse.json({"error": "Site not found"});
+            }
+
+        } catch (error) {
+            console.error(error);
+            return NextResponse.json({"error": "Something went wrong while saving data"});
+        }
+    
+    return NextResponse.json({"siteName": siteName});
 }
