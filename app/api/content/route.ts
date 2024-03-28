60c89fb53ac589e3cce18a6a65e1a52c161c07df
0x4E43 <nimaic.dev@gmail.com>
2024-03-28 22:54:35 +0530

    Get request added

--- app/api/content/route.ts
+++ app/api/content/route.ts
@@ -10,42 +10,71 @@ export async function POST(req: Request) {
         return NextResponse.json({"error": "No data found"});
     }
     const siteName = reqData.siteName;
-        try {
-            const site = await prisma.sites.findUnique({
+    try {
+        const site = await prisma.sites.findUnique({
+            where: {
+                name: siteName,
+            },
+            });
+        
+        if(site) {
+            const result = await prisma.notes.create({
+                data: {
+                    category: 'default', 
+                    title: reqData.title,
+                    content: reqData.content,
+                    siteId: site.id,
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                }
+            });
+
+            const resp : Notes= {
+                id: result.id,
+                category: result.category,
+                title: result.title,
+                content: result.content,
+                siteId: result.siteId
+            } 
+            return NextResponse.json({"message": "Data saved successfully", "data": resp});
+
+        }else{
+            return NextResponse.json({"error": "Site not found"});
+        }
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({"error": "Something went wrong while saving data"});
+    }
+   
+}
+
+export async function GET(req: Request) {
+    let siteName = req.headers.get('siteName') ?? undefined;
+    if(siteName === undefined && siteName !== null) {
+        return NextResponse.json({"error": "No data found"});
+    }
+    try {
+        const site = await prisma.sites.findUnique({
+            where: {
+                name: siteName,
+            },
+            });
+        console.log(site);
+        if(site) {
+            const notes = await prisma.notes.findMany({
                 where: {
-                    name: siteName,
+                    siteId: site.id,
                 },
                 });
-            
-            if(site) {
-                const result = await prisma.notes.create({
-                    data: {
-                        category: 'default', 
-                        title: reqData.title,
-                        content: reqData.content,
-                        siteId: site.id,
-                        createdAt: new Date(),
-                        updatedAt: new Date()
-                    }
-                });
-
-                const resp : Notes= {
-                    id: result.id,
-                    category: result.category,
-                    title: result.title,
-                    content: result.content,
-                    siteId: result.siteId
-                } 
-                return NextResponse.json({"message": "Data saved successfully", "data": resp});
-
-            }else{
-                return NextResponse.json({"error": "Site not found"});
-            }
-
-        } catch (error) {
-            console.error(error);
-            return NextResponse.json({"error": "Something went wrong while saving data"});
+            console.log(notes);
+            return NextResponse.json({"message": "Data fetched successfully", "data": notes});
+        }else{
+            return NextResponse.json({"error": "Site not found"});
         }
-    
-    return NextResponse.json({"siteName": siteName});
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({"error": "Something went wrong while fetching data"});
+    }
 }
