99a5edf705a60153f28614edd87fff8fc4708577
0x4E43 <nimaic.dev@gmail.com>
2024-03-31 23:55:31 +0530

    Added Card withHover effect

--- app/[user]/page.tsx
+++ app/[user]/page.tsx
@@ -1,13 +1,32 @@
 "use client";
 import UserNav from "./components/userNav";
 import AddTodo from "./components/addTodo";
+import { use, useEffect, useState } from "react";
+import { CardUI } from "./components/UI/cardUI";
 
 const UserPage = () => {
+  const [notes, setNotes] = useState<Notes[]>([]);
+
+  useEffect(() => {
+    fetch("/api/content", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        siteName: "hello",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.data);
+        setNotes(data.data);
+      });
+  }, []); // Correctly positioned the dependency array// Moved the dependency array outside of the `.then` method
   return (
     <>
       <UserNav />
       <div>
-            <AddTodo />
+        <AddTodo />
+        <CardUI items={notes} />
       </div>
     </>
   );
