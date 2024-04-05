d6338767d4a7c2babedd849c9dfdcebca339026e
0x4E43 <nimaic.dev@gmail.com>
2024-04-05 22:42:58 +0530

    Prompt password test

--- app/components/promptPassword.tsx
+++ app/components/promptPassword.tsx
@@ -1,24 +1,7 @@
 import React from "react";
 
 function PromptPassword() {
-  return (
-    <div>
-      <dialog id="my_modal_1" className="modal">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">
-            Press ESC key or click the button below to close
-          </p>
-          <div className="modal-action">
-            <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Close</button>
-            </form>
-          </div>
-        </div>
-      </dialog>
-    </div>
-  );
+  return <h1>Password Required</h1>;
 }
 
 export default PromptPassword;
