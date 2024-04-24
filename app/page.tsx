deedfd8145e357e327a445adee25ba16e5d43e10
0x4E43 <nimaic.dev@gmail.com>
2024-04-24 22:40:02 +0530

    page redisigned

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Landing from "@/components/landing";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1>Hello World</h1>
+        <Landing />
       </div>
     </main>
   );
