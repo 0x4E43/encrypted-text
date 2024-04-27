28a530c51f9324c28d207098ac90fde51ad2beb3
0x4E43 <nimaic.dev@gmail.com>
2024-04-27 23:11:11 +0530

    Toast added on click

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-between p-24">
-      <div>
+    <main className="flex h-screen flex-col items-center p-10">
+      <div className="flex items-center justify-between w-3/5  mx-10 lg:mx-30">
+        <div className="w-12 h-12 rounded-full bg-gray-200 "></div>
+        <div className="text-right">
+          <h1 className="text-4xl font-bold">Vault</h1>
+          <p className="text-gray-500">
+            A secure place to store your passwords
+          </p>
+        </div>
+      </div>
+      <div className="my-5 mt-5">
+        <h1>Some Random Info</h1>
+      </div>
+      <div className="mt-10">
         <Landing />
       </div>
     </main>
--- components/landing.tsx
+++ components/landing.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Button } from "./ui/button";
+import { useToast } from "@/components/ui/use-toast";
 import {
   Card,
   CardContent,
@@ -9,30 +10,39 @@ import {
   CardTitle,
 } from "./ui/card";
 import { Input } from "./ui/input";
+import { ToastAction } from "./ui/toast";
+import { Toaster } from "./ui/toaster";
 
 const Landing = () => {
+  const toast = useToast();
+  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("Clicked");
+    toast.toast({
+      title: "Scheduled: Catch up ",
+      description: "Friday, February 10, 2023 at 5:57 PM",
+      action: <ToastAction altText="Goto schedule to undo">Undo</ToastAction>,
+    });
+  };
   return (
     <div>
-      <div>
-        <h1 className="text-4xl font-bold">Welcome to Vault</h1>
-        <p className="text-lg text-gray-500">
-          A secure place to store your passwords
-        </p>
-      </div>
-
+      <Toaster />
       <div className="my-5">
         <Card className="w-[350px] lg:w-[500px]">
           <CardHeader>
             <CardTitle>Create your vault</CardTitle>
             <CardDescription>
-              We need a unique username to create your vault
+              We need an unique username to create your vault
             </CardDescription>
           </CardHeader>
           <CardContent>
             <Input placeholder="Enter your email" type="email" />
           </CardContent>
-          <CardFooter>
-            <Button className="" onClick={() => console.log("Button Clicked")}>
+          <CardFooter className="flex justify-end">
+            <Button
+              className="cursor-pointer"
+              onClick={handleOnClick}
+              disabled={false}
+            >
               Check Availability
             </Button>
           </CardFooter>
